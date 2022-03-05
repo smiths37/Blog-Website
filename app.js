c392b106e7a145e0883b5107679b19b4b95bf8ad
Sarah <smiths37@gmail.com>
2022-03-05 16:09:09 -0700

    Add data persistence (Mongoose)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,13 +19,77 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-const posts = []; //array to store all blog posts
+//const posts = []; //array to store all blog posts
+
+//Create a MongoDB Atlas database, schema, and model
+mongoose.connect("mongodb+srv://admin-sarah:Test123@cluster0.0ncxs.mongodb.net/blogDB");
+const postsSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+const Post = new mongoose.model("Post", postsSchema);
 
 //home/root route
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, function(err, posts){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
+    }
+  });
+
+  // res.render("home", {
+  //   homeStartingContent: homeStartingContent,
+  //   posts: posts
+  // });
+});
+
+//compose route
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+app.post("/compose", function(req, res) {
+  //store title and post body in a new object
+  // const post = {
+  //   title: req.body.postTitle,
+  //   content: req.body.postBody,
+  //   slug: _.kebabCase(req.body.postTitle)     //use lodash to replace spaces in title with dashes, use this for the post's url
+  // };
+  // //add the new post object to the posts array
+  // posts.push(post);
+
+  const post = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody
+  });
+
+  post.save(function(err){
+    if (!err){
+      //return to homepage
+      res.redirect("/");
+    }
+  });
+
+});
+
+//posts with parameters routes - dynamic URLs
+//check if route matches a title of a post
+//use lodash to ignore case and dashes
+app.get("/posts/:id", function(req, res) {
+  // posts.forEach(function(post) {
+  //   if (_.lowerCase(req.params.postName) === _.lowerCase(post.title)) {
+  //     res.render("post", {postTitle: post.title, postBody: post.content});
+  //   } else {
+  //     console.log("No match found :(");
+  //   }
+  // });
+  Post.findById(req.params.id, function(err, post){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("post", {postTitle: post.title, postBody: post.body});
+    }
   });
 });
 
@@ -41,40 +106,6 @@ app.get("/contact", function(req, res) {
     contactContent: contactContent
   });
 });
-
-//compose route
-app.get("/compose", function(req, res) {
-  res.render("compose");
-});
-app.post("/compose", function(req, res) {
-  //store title and post body in a new object
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-    slug: _.kebabCase(req.body.postTitle)     //use lodash to replace spaces in title with dashes, use this for the post's url
-  };
-  //add the new post object to the posts array
-  posts.push(post);
-  //return to homepage
-  res.redirect("/");
-});
-
-//posts with parameters routes - dynamic URLs
-//check if route matches a title of a post
-//use lodash to ignore case and dashes
-app.get("/posts/:postName", function(req, res) {
-  posts.forEach(function(post) {
-    if (_.lowerCase(req.params.postName) === _.lowerCase(post.title)) {
-      res.render("post", {postTitle: post.title, postBody: post.content});
-    } else {
-      console.log("No match found :(");
-    }
-  });
-});
-
-//don't want to list full post on first page - 100 characters and then ...
-
-
 
 
 app.listen(3000, function() {
